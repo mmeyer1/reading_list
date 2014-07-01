6009a11780196da7a715097c8b55ab787e1cc409
mmeyer1 <matt0719@gmail.com>
2014-06-30 22:51:36 -0400

    add books

---
+++ app/views/books/show.json.jbuilder
@@ -0,0 +1,1 @@
+json.extract! @book, :id, :title, :author, :descr, :amazon_id, :created_at, :updated_at
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,9 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+
+Book.destroy_all
+
+Book.create([{ title: 'Clash of Empires',
+  author: 'Dennis Showalter',
+  descr: "The classic history of the Tannenburg campaign, a case study in good military history",
+  amazon_id: '1574887815',
+  rating: 5,
+  finished_on: 10.months.ago}])
